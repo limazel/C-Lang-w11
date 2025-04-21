1e129bfdf34cffed64de11e68c5546c3d8f29149
Azad Ayhan <azadayhan50@gmail.com>
2025-04-21 11:25:28 +0300

    Demo 2 - Days of Year to Date

--- demo2.c
+++ demo2.c
@@ -29,8 +29,35 @@ int dayTransform(Date date) {
     return dayN;
 }
 
+Date dateTransform(int day, int year) {
+    Date d;
+    int month = 1;
+    int months[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if(artikYear(year)) {
+        months[2] = 29;
+    }
+    while(day > months[month]) {
+        day -= months[month];
+        month++;
+    }
+    d.day = day;
+    d.month = month;
+    d.year = year;
+    return d;
+}
+
+void printDate(Date date) {
+    printf("%d/%d/%d\n", date.day, date.month, date.year);
+}
+
 int main() {
     Date date= {1, 10, 2000};
     printf("Day number: %d\n", dayTransform(date));
+    int day, year;
+    printf("Enter day number and year: ");
+    scanf("%d %d", &day, &year);
+    Date d = dateTransform(day, year);
+    printf("Date: ");
+    printDate(d);
     return 0;
 }
