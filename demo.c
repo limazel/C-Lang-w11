0e23ee6d731ad1c7c7d06beafdb33c98ee180423
Azad Ayhan <azadayhan50@gmail.com>
2025-04-21 10:20:26 +0300

    Millisecond to Time

--- demo.c
+++ demo.c
@@ -14,6 +14,29 @@ long long milisaniye(Zaman z) {
     return ms;
 }
 
+int milisaniyeToZaman(long long ms, Zaman *z) {
+    z->ms = ms % 1000;
+    ms = ms / 1000;
+    z->s = ms % 60;
+    ms = ms / 60;
+    z->m = ms % 60;
+    ms = ms / 60;
+    z->h = ms;
+    return 0;
+}
+
+Zaman zaman(long long ms) {
+    Zaman result;
+    result.ms = ms % 1000;
+    ms = ms / 1000;
+    result.s = ms % 60;
+    ms = ms / 60;
+    result.m = ms % 60;
+    ms = ms / 60;
+    result.h = ms;
+    return result;
+}
+
 // Sonra fonksiyon tanımı
 Zaman zamanFark(Zaman z1, Zaman z2) {
     Zaman fark;
@@ -42,14 +65,19 @@ Zaman zamanFark(Zaman z1, Zaman z2) {
 
 int main() {
     Zaman z1, z2, fark;
-    printf("Zaman 1'i giriniz (hh:mm:ss:ms): ");
-    scanf("%d:%d:%d:%d", &z1.h, &z1.m, &z1.s, &z1.ms);
+    //printf("Zaman 1'i giriniz (hh:mm:ss:ms): ");
+    //scanf("%d:%d:%d:%d", &z1.h, &z1.m, &z1.s, &z1.ms);
     // printf("Zaman 2'yi giriniz (hh:mm:ss:ms): ");
     // scanf("%d:%d:%d:%d", &z2.h, &z2.m, &z2.s, &z2.ms);
     
     // fark = zamanFark(z1, z2);
     // printf("Zaman farki: %02d:%02d:%02d:%03d\n", fark.h, fark.m, fark.s, fark.ms);
-    printf("%lld misil saniye\n", milisaniye(z1));
+    //printf("%lld misil saniye\n", milisaniye(z1));
+
+    printf("Milisaniye cinsinden zaman 1'i giriniz: ");
+    long long ms1;
+    scanf("%lld", &ms1);
+    printf("%lld milisaniye %d saat %d dakika %d saniye %d milisaniye\n", ms1, zaman(ms1).h, zaman(ms1).m, zaman(ms1).s, zaman(ms1).ms);
     return 0;
 }
 // Bu kod, iki zaman arasındaki farkı hesaplar ve sonucu ekrana yazdırır.
